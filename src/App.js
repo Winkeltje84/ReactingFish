0f0477e11d2e7d2c0bcaaafe2699a7cbe470a34e
Winkeltje84 <bwinkel@gmail.com>
2017-05-22 15:11:26 +0200

    made a Title component, and added tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Title from './components/Title'
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      // <h1>Hello World!</h1>
+      <div>
+        <Title content="Hello fishies" />
+        <Title content="Oh how beautifull you are!" />
+      </div>
     )
   }
 }
