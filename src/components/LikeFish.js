4833315f7878c86aed08ff55210f396928844de3
Winkeltje84 <bwinkel@gmail.com>
2017-05-27 17:47:32 +0200

    added a like button & sass that shows a like button. When clicking on it the console shows "like button clicked" but nothing else happens still

---
+++ src/components/LikeFish.js
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react'
+import './LikeFish.sass'
+
+class LikeFish extends PureComponent {
+  constructor() {
+    super()
+
+    this.state = {
+      liked: false
+    }
+  }
+
+  classNames() {
+    const { liked } = this.state
+    let classes = 'like'
+
+    if (liked) { classes += ' liked' }
+
+    return classes
+  }
+
+  toggleLike() {
+    console.log('Like button clicked!')
+  }
+
+  render() {
+    const liked = false
+    return (
+      <p className={ this.classNames() }>
+        <button onClick={ this.toggleLike.bind(this) }>
+          { liked ? '❤️' : '♡' }
+        </button>
+        <span className="likes">{ liked ? 'You like this' : null }
+        </span>
+      </p>
+    )
+  }
+}
+
+export default LikeFish
