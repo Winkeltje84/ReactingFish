fa4804020dfd297cbf784e0d7820ef5e3fd719a1
Winkeltje84 <bwinkel@gmail.com>
2017-05-29 12:24:55 +0200

    minor visual changes & changed <p> in more logical <div>

--- src/components/LikeFish.test.js
+++ src/components/LikeFish.test.js
@@ -9,8 +9,12 @@ chai.use(chaiEnzyme())
 describe('<LikeFish />', () => {
   const button = shallow(<LikeFish />)
 
-  it('is wrapped in a <p> with class "like"', () => {
-    expect(button).to.have.tagName('p')
+  it('is wrapped in a <div> with class "like"', () => {
+    expect(button).to.have.tagName('div')
     expect(button).to.have.className('like')
+  })
+
+  it('has an initial state for "liked", that is false', () => {
+    expect(button.state('liked')).to.eq(false)
   })
 })
